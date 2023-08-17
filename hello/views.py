5c9a1ea31e90d003211cf835d595220415e7cf71
Luciano Filho <lvgalvaofilho@gmail.com>
2023-08-17 06:28:46 -0300

    Adicionando o comentário fora do padrao UTF-8

--- hello/views.py
+++ hello/views.py
@@ -6,6 +6,6 @@ def hello_world(request):
     python_version = platform.python_version()
     django_version = django.get_version()
 
-    return HttpResponse("Hello, World!<br>Tudo funcionando bem<br>"
+    return HttpResponse("Hello, World!<br>Tudo funcionando bem até agora<br>"
                         "--Python Version: {}<br>"
                         "--Django Version: {}".format(python_version, django_version))
