fdeb11f33b046fd07d78cac948c048228ee7413c
Luciano Filho <lvgalvaofilho@gmail.com>
2023-08-18 09:28:39 -0300

    agora é comentário

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,11 @@ import django
 def hello_world(request):
     python_version = platform.python_version()
     django_version = django.get_version()
+    
+    
+    # Gabriel olha aqui o seu comentário quabrando tudo, bjo
 
     return HttpResponse("Hello, World!<br>Tudo funcionando bem<br>"
                         "--Python Version: {}<br>"
                         "--Django Version: {}".format(python_version, django_version))
+
